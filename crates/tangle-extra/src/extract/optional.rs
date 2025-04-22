6d4e9e127371d86afc0146a50601d8c81894aef8
Alex <69764315+Serial-ATA@users.noreply.github.com>
2025-04-22 14:59:31 -0400

    feat(tangle-extra): derive more traits for `List` and `Optional` (#912)

--- crates/tangle-extra/src/extract/optional.rs
+++ crates/tangle-extra/src/extract/optional.rs
@@ -1,9 +1,40 @@
+use core::fmt::Debug;
 use serde::{Deserialize, Serialize, Serializer};
 
 // TODO(serial): Document, warn against using `Option<T>` in tangle jobs
-#[derive(Deserialize)]
+#[derive(Deserialize, Copy, Eq, Hash)]
 #[serde(transparent)]
 pub struct Optional<T: Default>(pub Option<T>);
+
+impl<T: PartialEq + Default> PartialEq for Optional<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.eq(&other.0)
+    }
+}
+
+impl<T: Debug + Default> Debug for Optional<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl<T: PartialOrd + Default> PartialOrd for Optional<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl<T: Ord + Default> Ord for Optional<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+
+impl<T: Clone + Default> Clone for Optional<T> {
+    fn clone(&self) -> Self {
+        Optional(self.0.clone())
+    }
+}
 
 impl<T: Default> Default for Optional<T> {
     fn default() -> Self {
