f6b94775f6a743bab0230c4528e94f7da81975a8
Serial <69764315+Serial-ATA@users.noreply.github.com>
2025-06-04 00:59:10 -0400

    wip

--- crates/manager/src/rt/hypervisor/images.rs
+++ crates/manager/src/rt/hypervisor/images.rs
@@ -11,7 +11,6 @@ const VMLINUZ_URL: &str = "https://cloud-images.ubuntu.com/releases/releases/24.
 const INITRD_URL: &str = "https://cloud-images.ubuntu.com/releases/releases/24.04/release/unpacked/ubuntu-24.04-server-cloudimg-amd64-initrd-generic";
 
 const CLOUD_IMG_FILE_NAME: &str = "ubuntu.img";
-const CLOUD_IMG_RAW_FILE_NAME: &str = "ubuntu-raw.img";
 const VMLINUZ_IMG_FILE_NAME: &str = "vmlinuz";
 const INITRD_IMG_FILE_NAME: &str = "initrd.img";
 
@@ -21,44 +20,24 @@ const FILES: &[(&str, &str)] = &[
     (INITRD_IMG_FILE_NAME, INITRD_URL),
 ];
 
+// Give each VM a 20GB rootfs
+const ALLOCATED_IMAGE_SIZE: u64 = 1024 * 1024 * 1024 * 20;
+
 async fn download_image_if_needed(cache_dir: impl AsRef<Path>) -> Result<()> {
     let cache = cache_dir.as_ref();
 
-    let mut downloaded_cloud_image = false;
-    for (dest_file_name, url) in FILES {
+    for (dest_file_name, url) in FILES.iter().copied() {
         let dest = cache.join(dest_file_name);
         if !dest.exists() {
             warn!("{} not found, downloading...", dest.display());
 
-            let response = reqwest::get(*url).await?.bytes().await?;
+            let response = reqwest::get(url).await?.bytes().await?;
             let mut f = fs::OpenOptions::new()
                 .write(true)
                 .create(true)
                 .truncate(true)
-                .open(dest)?;
+                .open(&dest)?;
             f.write_all(&response)?;
-
-            if *dest_file_name == CLOUD_IMG_FILE_NAME {
-                downloaded_cloud_image = true;
-            }
-        }
-    }
-
-    // cloud-hypervisor doesn't support compressed QCOW2, need to convert to raw
-    if downloaded_cloud_image {
-        let compressed_img_path = cache.join(CLOUD_IMG_FILE_NAME);
-        let raw_img_path = cache.join(CLOUD_IMG_RAW_FILE_NAME);
-        let out = Command::new("qemu-img")
-            .args(["convert", "-O", "raw"])
-            .arg(compressed_img_path)
-            .arg(raw_img_path)
-            .output()?;
-
-        if !out.status.success() {
-            return Err(Error::Other(format!(
-                "Failed to convert Ubuntu cloud image to raw: {}",
-                String::from_utf8_lossy(&out.stderr)
-            )));
         }
     }
 
@@ -76,8 +55,35 @@ impl CloudImage {
         let cache = cache_dir.as_ref();
         download_image_if_needed(cache).await?;
 
-        let image_dest = data_dir.as_ref().join(CLOUD_IMG_RAW_FILE_NAME);
-        fs::copy(cache.join(CLOUD_IMG_RAW_FILE_NAME), &image_dest)?;
+        let base_image_src = cache.join(CLOUD_IMG_FILE_NAME);
+        let image_dest = data_dir.as_ref().join(CLOUD_IMG_FILE_NAME);
+
+        // The Ubuntu cloud images are compressed QCOW2, which cloud-hypervisor doesn't support
+        let out = Command::new("qemu-img")
+            .args(["convert", "-O", "raw"])
+            .arg(base_image_src)
+            .arg(&image_dest)
+            .output()?;
+
+        if !out.status.success() {
+            return Err(Error::Other(format!(
+                "Failed to convert Ubuntu cloud image to raw: {}",
+                String::from_utf8_lossy(&out.stderr)
+            )));
+        }
+
+        let out = Command::new("qemu-img")
+            .arg("resize")
+            .arg(&image_dest)
+            .arg(ALLOCATED_IMAGE_SIZE.to_string())
+            .output()?;
+
+        if !out.status.success() {
+            return Err(Error::Other(format!(
+                "Failed to resize image: {}",
+                String::from_utf8_lossy(&out.stderr)
+            )));
+        }
 
         Ok(Self {
             image: image_dest,
