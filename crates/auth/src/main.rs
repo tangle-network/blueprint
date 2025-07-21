3be450f30da7399229423f44a98bbafe0dd2b464
Shady Khalifa <dev+github@shadykhalifa.me>
2025-07-22 01:08:36 +0300

    fix: insert the default service

--- crates/auth/src/main.rs
+++ crates/auth/src/main.rs
@@ -1,6 +1,7 @@
 use std::net::IpAddr;
 
 use blueprint_auth::proxy::DEFAULT_AUTH_PROXY_PORT;
+use blueprint_auth::types::ServiceId;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -12,6 +13,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let auth_proxy_host = IpAddr::V4(std::net::Ipv4Addr::UNSPECIFIED);
 
     let proxy = blueprint_auth::proxy::AuthenticatedProxy::new(&db_path)?;
+
+    blueprint_auth::models::ServiceModel {
+        api_key_prefix: String::from("mcp_test"),
+        owners: vec![blueprint_auth::models::ServiceOwnerModel {
+            key_type: blueprint_auth::types::KeyType::Ecdsa as _,
+            // Alice's public key in hex format
+            key_bytes: hex::decode(
+                "020a1091341fe5664bfa1782d5e04779689068c916b04cb365ec3153755684d9a1",
+            )
+            .expect("Failed to decode hex public key"),
+        }],
+        // The URL of the upstream service that this proxy will forward requests to
+        upstream_url: String::from("http://localhost:3000"),
+    }
+    .save(ServiceId::new(0), &proxy.db())?;
 
     let router = proxy.router();
 
